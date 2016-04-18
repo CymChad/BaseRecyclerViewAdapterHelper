3942b81e4865b085abd97acc5d90c66086dee455
chad <54050520@qq.com>
2016-04-18 11:44:02 +0800

    add sectionEntity

---
+++ library/src/main/java/com/chad/library/adapter/base/entity/SectionEntity.java
@@ -0,0 +1,19 @@
+package com.chad.library.adapter.base.entity;
+
+public abstract class SectionEntity<T> {
+    public boolean isHeader;
+    public T t;
+    public String header;
+
+    public SectionEntity(boolean isHeader, String header) {
+        this.isHeader = isHeader;
+        this.header = header;
+        this.t = null;
+    }
+
+    public SectionEntity(T t) {
+        this.isHeader = false;
+        this.header = null;
+        this.t = t;
+    }
+}
