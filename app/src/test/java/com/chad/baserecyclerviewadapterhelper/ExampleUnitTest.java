67851e2e57afe409f92d5ef9cf6670ccef3387e7
CymChad <it_chenyuming@163.com>
2016-05-30 00:45:34 +0800

    Update ExampleUnitTest.java

--- app/src/test/java/com/chad/baserecyclerviewadapterhelper/ExampleUnitTest.java
+++ app/src/test/java/com/chad/baserecyclerviewadapterhelper/ExampleUnitTest.java
@@ -1,8 +1,5 @@
 package com.chad.baserecyclerviewadapterhelper;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
