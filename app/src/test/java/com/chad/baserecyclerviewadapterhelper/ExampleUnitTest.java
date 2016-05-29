6257d40b7129187691b245efcb3c4ca8c358fbe0
CymChad <it_chenyuming@163.com>
2016-05-30 00:25:06 +0800

    Update ExampleUnitTest.java

--- app/src/test/java/com/chad/baserecyclerviewadapterhelper/ExampleUnitTest.java
+++ app/src/test/java/com/chad/baserecyclerviewadapterhelper/ExampleUnitTest.java
@@ -8,8 +8,5 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+    
 }
