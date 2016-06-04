b4da869ee5a8eba7bd0241e5efa3fb7d8544a3a8
zeng1990java <zeng1990java@gmail.com>
2016-06-04 22:05:13 +0800

    fix load more bug  when loading: ArrayIndexOutOfBoundsException. and add load more use sample

--- app/src/main/java/com/chad/baserecyclerviewadapterhelper/HomeActivity.java
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/HomeActivity.java
@@ -18,9 +18,9 @@ import java.util.ArrayList;
  * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
  */
 public class HomeActivity extends Activity {
-    private static final Class<?>[] ACTIVITY = {AnimationUseActivity.class, MultipleItemUseActivity.class, HeaderAndFooterUseActivity.class, PullToRefreshUseActivity.class, SectionUseActivity.class, EmptyViewUseActivity.class};
-    private static final String[] TITLE = {"Animation Use", "MultipleItem Use", "HeaderAndFooter Use", "PullToRefresh Use", "Section Use", "EmptyView Use"};
-    private static final String[] COLOR_STR = {"#0dddb8","#0bd4c3","#03cdcd","#00b1c5","#04b2d1","#04b2d1","#04b2d1"};
+    private static final Class<?>[] ACTIVITY = {AnimationUseActivity.class, MultipleItemUseActivity.class, HeaderAndFooterUseActivity.class, PullToRefreshUseActivity.class, SectionUseActivity.class, EmptyViewUseActivity.class, LoadMoreActivity.class};
+    private static final String[] TITLE = {"Animation Use", "MultipleItem Use", "HeaderAndFooter Use", "PullToRefresh Use", "Section Use", "EmptyView Use", "LoadMore Use"};
+    private static final String[] COLOR_STR = {"#0dddb8","#0bd4c3","#03cdcd","#00b1c5","#04b2d1","#04b2d1","#04b2d1", "#01b9c4"};
     private ArrayList<HomeItem> mDataList;
     private RecyclerView mRecyclerView;
 
--- library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
+++ library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
@@ -470,7 +470,7 @@ public abstract class BaseQuickAdapter<T> extends RecyclerView.Adapter<RecyclerV
 
 
     private void addLoadMore(RecyclerView.ViewHolder holder) {
-        if (isLoadMore()) {
+        if (isLoadMore() && !mLoadingMoreEnable) {
             mLoadingMoreEnable = true;
             mRequestLoadMoreListener.onLoadMoreRequested();
         }
@@ -518,7 +518,7 @@ public abstract class BaseQuickAdapter<T> extends RecyclerView.Adapter<RecyclerV
     }
 
     private boolean isLoadMore() {
-        return mNextLoadEnable && pageSize != -1 && !mLoadingMoreEnable && mRequestLoadMoreListener != null && mData.size() >= pageSize;
+        return mNextLoadEnable && pageSize != -1 && mRequestLoadMoreListener != null && mData.size() >= pageSize;
     }
 
     protected View getItemView(int layoutResId, ViewGroup parent) {
