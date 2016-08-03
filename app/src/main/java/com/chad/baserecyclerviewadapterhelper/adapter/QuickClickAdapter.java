359528ae08a11d15b5daa7acbe2715d610801453
Allen <allen@Allen-MacPro.local>
2016-08-03 15:53:22 +0800

    提交 点击 长按 child click 事件

---
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/adapter/QuickClickAdapter.java
@@ -0,0 +1,41 @@
+package com.chad.baserecyclerviewadapterhelper.adapter;
+
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.chad.baserecyclerviewadapterhelper.R;
+import com.chad.baserecyclerviewadapterhelper.data.DataServer;
+import com.chad.baserecyclerviewadapterhelper.entity.Status;
+import com.chad.baserecyclerviewadapterhelper.transform.GlideCircleTransform;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public class QuickClickAdapter extends BaseQuickAdapter<Status> {
+    public QuickClickAdapter() {
+        super( R.layout.tweet, DataServer.getSampleData(100));
+    }
+
+    public QuickClickAdapter(int dataSize) {
+        super( R.layout.tweet, DataServer.getSampleData(dataSize));
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Status item) {
+        helper.setText(R.id.tweetName, item.getUserName())
+                .setText(R.id.tweetText, item.getText())
+                .setText(R.id.tweetDate, item.getCreatedAt())
+                .setVisible(R.id.tweetRT, item.isRetweet())
+                .addOnClickListener(R.id.tweetAvatar)
+                .addOnClickListener(R.id.tweetName)
+                .linkify(R.id.tweetText);
+        Log.d(TAG, "convert: "+R.id.tweetAvatar);
+        Log.d(TAG, "convert: "+R.id.tweetName);
+        Glide.with(mContext).load(item.getUserAvatar()).crossFade().placeholder(R.mipmap.def_head).transform(new GlideCircleTransform(mContext)).into((ImageView) helper.getView(R.id.tweetAvatar));
+    }
+
+
+}
