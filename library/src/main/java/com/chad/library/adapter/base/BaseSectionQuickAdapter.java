3753e7bb6231a3415d89233b850ce43340a2b875
chad <54050520@qq.com>
2017-03-29 16:45:41 +0800

    fixbug #933

--- library/src/main/java/com/chad/library/adapter/base/BaseSectionQuickAdapter.java
+++ library/src/main/java/com/chad/library/adapter/base/BaseSectionQuickAdapter.java
@@ -54,6 +54,6 @@ public abstract class BaseSectionQuickAdapter<T extends SectionEntity, K extends
         }
     }
 
-    protected abstract void convertHead(BaseViewHolder helper, T item);
+    protected abstract void convertHead(K helper, T item);
 
 }
