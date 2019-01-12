84301f0f0d402c8ea5d3441565ebb919f5c441f5
AllenCoder <allencoder@126.com>
2019-01-12 20:52:59 +0800

    merge master to androidx branch

--- app/src/main/java/com/chad/baserecyclerviewadapterhelper/decoration/GridSectionAverageGapItemDecoration.java
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/decoration/GridSectionAverageGapItemDecoration.java
@@ -2,8 +2,6 @@ package com.chad.baserecyclerviewadapterhelper.decoration;
 
 import android.graphics.Rect;
 import android.os.Build;
-import androidx.widget.GridLayoutManager;
-import androidx.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.View;
@@ -12,6 +10,9 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseSectionQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.chad.library.adapter.base.entity.SectionEntity;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * 应用于RecyclerView的GridLayoutManager，水平方向上固定间距大小，从而使条目宽度自适应。<br>
