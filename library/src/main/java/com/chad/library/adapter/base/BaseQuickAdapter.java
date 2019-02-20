4f620b4e05c4a05ef3f96854c54d4f64c2779381
Allen <AllenCoder@126.com>
2019-02-20 16:01:24 +0800

    1.fix document empty method body

--- library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
+++ library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
@@ -1946,7 +1946,7 @@ public abstract class BaseQuickAdapter<T, K extends BaseViewHolder> extends Recy
         /**
          * callback method to be invoked when an item in this view has been
          * click and held
-         *
+         * @param adapter  this BaseQuickAdapter adapter
          * @param view     The childView whihin the itemView that was clicked and held.
          * @param position The position of the view int the adapter
          * @return true if the callback consumed the long click ,false otherwise
