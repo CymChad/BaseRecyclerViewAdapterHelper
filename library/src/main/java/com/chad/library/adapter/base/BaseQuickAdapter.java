d4b44cb187e58947b7e46909bb2d52aec51d9bac
xxjy <xxjy.zzz@gmail.com>
2019-06-10 10:54:08 +0800

    fix setEmptyView crash because mHeadAndEmptyEnable, mFootAndEmptyEnable, setEmptyView may not change itemCount

--- library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
+++ library/src/main/java/com/chad/library/adapter/base/BaseQuickAdapter.java
@@ -1370,6 +1370,7 @@ public abstract class BaseQuickAdapter<T, K extends BaseViewHolder> extends Recy
     }
 
     public void setEmptyView(View emptyView) {
+        int oldItemCount = getItemCount();
         boolean insert = false;
         if (mEmptyLayout == null) {
             mEmptyLayout = new FrameLayout(emptyView.getContext());
@@ -1390,7 +1391,11 @@ public abstract class BaseQuickAdapter<T, K extends BaseViewHolder> extends Recy
             if (mHeadAndEmptyEnable && getHeaderLayoutCount() != 0) {
                 position++;
             }
-            notifyItemInserted(position);
+            if (getItemCount() > oldItemCount) {
+                notifyItemInserted(position);
+            } else {
+                notifyDataSetChanged();
+            }
         }
     }
 
